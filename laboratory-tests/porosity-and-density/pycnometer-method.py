d5de2db742855427452eca5d577978a90940da38
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-11-27 00:37:41 +0300

    add total porosity function for pycnometer

--- laboratory-tests/porosity-and-density/pycnometer-method.py
+++ laboratory-tests/porosity-and-density/pycnometer-method.py
@@ -6,3 +6,9 @@
 def mineral_grain_density(p1,p2,p3,p4,dw):
     mgd = ((p2-p1)*dw)/(p4+p2-p1-p3)
     return mgd
+
+#mg->mineral grain density
+#md->dry density
+def total_porosity_percentage(mg,md):
+    tp = ((mg-md)/mg)*100
+    return tp
