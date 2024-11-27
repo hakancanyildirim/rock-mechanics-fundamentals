6e420f7ea8f6d73e4b94ce78883c1ac52347ea38
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-11-27 13:23:09 +0300

    add common methods to buoyancy method

---
+++ laboratory-tests/porosity-and-density/buoyancy-method.py
@@ -0,0 +1,24 @@
+#pv->pore volume
+#bv->bulk volume
+def effective_porosity(pv, bv):
+    n = (pv/bv)*100
+    return n
+
+#wsat->saturated weight
+#gw->mineral grain weight
+#pw->density of water
+def pore_volume(wsat, gw, pw):
+    pv = (wsat-gw)/pw
+    return pv
+
+#gw->mineral grain weight
+#bv->bulk volume
+def dry_density(gw, bv):
+    µd = gw/bv
+    return µd
+
+#wsat->saturated weight
+#bv->bulk volume
+def saturated_density(wsat, bv):
+    µsat = wsat/bv
+    return µsat
