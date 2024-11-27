b7f721fd87eb379b3cdd721372b289ff1b8b6ffc
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-11-27 14:43:07 +0300

    init cohesion function for strength envelope

--- laboratory-tests/triaxial-compression.py/strength-envelope.py
+++ laboratory-tests/triaxial-compression.py/strength-envelope.py
@@ -1,7 +1,12 @@
 from math import sin, cos, acos, asin
 
 #m->gradient of the strength envelope
-#b->Y intercept of the strength envelope
 def internal_friction_angle(m):
     fi = asin((m-1)/(m+1))
     return fi
+
+#fi->internal friction angle
+#b->Y intercept of the strength envelope
+def cohesion(fi, b):
+    c = b*((1-sin(fi))/(2*cos(fi)))
+    return c
