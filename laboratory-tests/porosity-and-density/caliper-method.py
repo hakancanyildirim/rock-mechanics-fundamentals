553605efbe1d8d7bc678962bdd2bf576862d5318
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-11-27 12:50:53 +0300

    add effective porosity function for caliper method

--- laboratory-tests/porosity-and-density/caliper-method.py
+++ laboratory-tests/porosity-and-density/caliper-method.py
@@ -5,3 +5,9 @@ def bulk_volume(diameter, length):
     area = pi * (diameter)**2
     bv = area * length
     return bv
+
+#pv->pore volume
+#bv->bulk volume
+def effective_porosity(pv, bv):
+    n = (pv/bv)*100
+    return n
