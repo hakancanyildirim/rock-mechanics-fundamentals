9327139d5552ab4069934052dd9a54b8347550cf
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-11-27 12:57:06 +0300

    add pore volume function for caliper method

--- laboratory-tests/porosity-and-density/caliper-method.py
+++ laboratory-tests/porosity-and-density/caliper-method.py
@@ -11,3 +11,10 @@ def bulk_volume(diameter, length):
 def effective_porosity(pv, bv):
     n = (pv/bv)*100
     return n
+
+#wsat->saturated weight
+#gw->mineral grain weight
+#pw->density of water
+def pore_volume(wsat, gw, pw):
+    pv = (wsat-gw)/pw
+    return pv
