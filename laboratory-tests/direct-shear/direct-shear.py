e5d16903bf366d8d9bae7dc1ee65b1db7556ca57
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-12-03 23:03:37 +0300

    create normal stress on sheared plane function for direct shear test

--- laboratory-tests/direct-shear/direct-shear.py
+++ laboratory-tests/direct-shear/direct-shear.py
@@ -8,3 +8,11 @@ def shear_stress_on_sheared_plane(tau, i, sigma):
     taum = tau*cos(i)-sigma*sin(i)
     return taum
 
+#tau->shear stress
+#i->angle of incidence of the projections
+#sigma->normal stress
+#sigmam->normal stress on the sheared plane
+def normal_stress_on_sheared_plane(tau, i, sigma):
+    sigmam = tau*sin(i)+sigma*cos(i)
+    return sigmam
+
