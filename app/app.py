e33bc099b19b73fddb635cb12d974668c6d16afd
hakan <77567561+hakancanyildirim@users.noreply.github.com>
2024-12-17 15:40:24 +0300

    refactor

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-
 import sys
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Rock Mechanics Toolkit")
+        #button = QPushButton("testing")
+        #self.setCentralWidget(button)
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
